d102cd85d7a58a030bf1905a3acc446121e77f36
fcostantinicatenate <federico.costantini@catenate.com>
2022-12-09 19:00:56 +0100

    prova sprite a cazzo

--- test/test.js
+++ test/test.js
@@ -1,7 +1,15 @@
-bottom = 350;
+// import { spriteMovements,startAnimation} from '../prova-sprite/sprite.js'
+var bottom = 350;
 var t = Date.now();
 var speed = 20;
 var cont = 0;
+
+
+var animationInterval;
+// var spriteSheet = document.getElementById("sprite-image");
+var widthOfSpriteSheet = 832; //sbagliati
+var widthOfEachSprite = 832 / 13;
+var heightOfEachSprite = 64; //px
 
 //coordinates 
 var x = 0;
@@ -9,6 +17,11 @@ var y = bottom;
 //prendo il canvas dall'HTML________________________________________________________________
 var canvas = document.getElementById("canvas");
 var mySquare = canvas.getContext("2d");
+var spriteImage = document.createElement("div");
+spriteImage.classList.add("sprite-image")
+var gameWrapper = document.getElementById("game-wrapper")
+gameWrapper.appendChild(spriteImage)
+
 //________________________________________________________________________________________________________________________________
 
 //generazione componente da js ___________________________________________________________________
@@ -24,6 +37,7 @@ var movements = {
   goingLeft: false,
   isJumping: false,
 };
+
 
 //L I S T N E N E R S ----------------------------------------------------------------
 window.addEventListener("keydown", lateralMovement, false);
@@ -47,6 +61,7 @@ function goRight() {
   if (movements.goingRight) {
     x += speed;
     draw();
+    // drawSprite(sprite.spriteMovements.walkDx);
     console.log("going right");
     window.requestAnimationFrame(goRight);
   }
@@ -56,6 +71,8 @@ function goLeft() {
   if (movements.goingLeft) {
     x -= speed;
     draw();
+    
+    // drawSprite(sprite.spriteMovements.walkSx);
     console.log("going left");
     window.requestAnimationFrame(goLeft);
   }
@@ -66,6 +83,7 @@ function jump() {
   if (cont < 20) {
     y -= 10;
     draw();
+    drawSprite();
     cont++;
     movements.isJumping = true;
     window.requestAnimationFrame(jump);
@@ -82,12 +100,10 @@ function stopMove(e) {
     case 37:
       movements.goingLeft = false;
       console.log("left key released");
-      window.cancelAnimationFrame(goLeft());
       break;
     case 39:
       movements.goingRight = false;
       console.log("right key released");
-      window.cancelAnimationFrame(goRight());
       break;
   }
 }
@@ -120,6 +136,7 @@ function gravity() {
   if (!movements.isJumping) {
     y += speed / 2;
     draw(); 
+    drawSprite();
   }
   window.requestAnimationFrame(gravity);
 }
@@ -133,17 +150,56 @@ function draw() {
   mySquare.fillStyle = "red";
   mySquare.fill();
   mySquare.closePath();
+ 
+}
 
-  // ctx.beginPath();
-  // borderCheck();
-  // ctx.rect(10, 10, 100, 100);
-  // ctx.fillStyle = "green";
-  // ctx.fill();
-  // ctx.closePath(); 
+function drawSprite(movement) {
+  spriteImage.style.left = x+"px"
+  spriteImage.style.top = y+"px"
+  startAnimation(movement,spriteImage)
 }
 
 window.onload = function () {
   //var t = Date.now();
+  console.log(spriteMovements)
   gravity();
   draw();
+  drawSprite(spriteMovements.walkSx);
 };
+
+
+export const spriteMovements = {
+  walkDx: {
+    row: 10, //every movement has some properties
+    numOfImg: 9,
+  },
+  walkSx: {
+    row: 12,
+    numOfImg: 9,
+  },
+};
+
+// ANIMATIONS FUNCTION___________________________________________________________________
+
+export function startAnimation(movement,spriteSheet) {
+  var position = widthOfEachSprite; //start position for the image
+  const speed = 100; //in millisecond(ms)
+  const diff = widthOfEachSprite; //difference between two sprites
+
+  animationInterval = setInterval(() => {
+    spriteSheet.style.backgroundPosition = `-${position}px ${heightOfEachSprite * movement.row}px`;
+
+    drawSprite(sprite.spriteMovements.walkSx);
+    if (position < widthOfSpriteSheet) {
+      position = position + diff;
+    } else {
+      //increment the position by the width of each sprite each time
+      position = widthOfEachSprite;
+    }
+    if (position == widthOfEachSprite * movement.numOfImg) {
+      //
+      position = widthOfEachSprite;
+    }
+    //reset the position to show first sprite after the last one
+  }, speed);
+}
