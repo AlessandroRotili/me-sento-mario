a114818bb0dea7b125cb7449780aec0f419daebb
Alessandro Rotili <a.rotili94@gmail.com>
2022-12-09 11:44:40 +0100

    Generic animations function

--- prova-sprite/sprite.js
+++ prova-sprite/sprite.js
@@ -1,22 +1,34 @@
 var animationInterval;
 var spriteSheet = document.getElementById("sprite-image");
 var widthOfSpriteSheet = 832; //sbagliati
-var widthOfEachSprite = 832/13;
+var widthOfEachSprite = 832 / 13;
 var heightOfEachSprite = 64; //px
 
 function stopAnimation() {
   clearInterval(animationInterval);
 }
 
-// ___________________________________________________________________
+//List of movements
+spriteMovements = {
+  walkDx: {
+    row: 10, //every movement ha some properties
+    numOfImg: 9,
+  },
+  walkSx: {
+    row: 12,
+    numOfImg: 9,
+  },
+};
 
-function startAnimation() {
+// ANIMATIONS FUNCTION___________________________________________________________________
+
+function startAnimation(movement) {
   var position = widthOfEachSprite; //start position for the image
   const speed = 100; //in millisecond(ms)
   const diff = widthOfEachSprite; //difference between two sprites
 
   animationInterval = setInterval(() => {
-    spriteSheet.style.backgroundPosition = `-${position}px ${heightOfEachSprite*2}px`;
+    spriteSheet.style.backgroundPosition = `-${position}px ${heightOfEachSprite * movement.row}px`;
 
     if (position < widthOfSpriteSheet) {
       position = position + diff;
@@ -24,36 +36,14 @@ function startAnimation() {
       //increment the position by the width of each sprite each time
       position = widthOfEachSprite;
     }
+    if (position == widthOfEachSprite * movement.numOfImg) {
+      //
+      position = widthOfEachSprite;
+    }
     //reset the position to show first sprite after the last one
   }, speed);
 }
 
-//prova camminata ___________________________________________________________________
-function walking() {
-  var position = widthOfEachSprite; //start position for the image
-  const speed = 100; //in millisecond(ms)
-  const diff = widthOfEachSprite; //difference between two sprites
-  const numberOfImagesForWalk = 9;
-
-    animationInterval = setInterval(() => {
-      spriteSheet.style.backgroundPosition = `-${position}px ${heightOfEachSprite*10}px`;
-  
-      if (position < widthOfSpriteSheet) {
-        position = position + diff;
-      } else {
-        //increment the position by the width of each sprite each time
-        position = widthOfEachSprite;
-      }
-      if(position == widthOfEachSprite*numberOfImagesForWalk){ //
-        position = widthOfEachSprite;
-      }
-      //reset the position to show first sprite after the last one
-    }, speed);
-
-  }
-
-
 //___________________________________________________________________
 //Start animation
-// startAnimation();
-walking();
+startAnimation(spriteMovements.walkSx);
