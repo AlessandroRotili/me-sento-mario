a002c564366f8fe1c94cf1c1bbfb875351679e33
Alessandro Rotili <a.rotili94@gmail.com>
2022-12-09 11:24:48 +0100

    walking function sample

--- prova-sprite/sprite.js
+++ prova-sprite/sprite.js
@@ -27,6 +27,33 @@ function startAnimation() {
     //reset the position to show first sprite after the last one
   }, speed);
 }
+
+//prova camminata ___________________________________________________________________
+function walking() {
+  var position = widthOfEachSprite; //start position for the image
+  const speed = 100; //in millisecond(ms)
+  const diff = widthOfEachSprite; //difference between two sprites
+  const numberOfImagesForWalk = 9;
+
+    animationInterval = setInterval(() => {
+      spriteSheet.style.backgroundPosition = `-${position}px ${heightOfEachSprite*10}px`;
+  
+      if (position < widthOfSpriteSheet) {
+        position = position + diff;
+      } else {
+        //increment the position by the width of each sprite each time
+        position = widthOfEachSprite;
+      }
+      if(position == widthOfEachSprite*numberOfImagesForWalk){ //
+        position = widthOfEachSprite;
+      }
+      //reset the position to show first sprite after the last one
+    }, speed);
+
+  }
+
+
 //___________________________________________________________________
 //Start animation
-startAnimation();
+// startAnimation();
+walking();
