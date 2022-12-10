221737da44a580a5023cb08d23e0d7cd08c01c74
Alessandro Rotili <a.rotili94@gmail.com>
2022-12-10 01:22:51 +0100

    .

--- prova-sprite/sprite.js
+++ prova-sprite/sprite.js
@@ -1,46 +1,49 @@
+var animationInterval;
+var spriteSheet = document.getElementById("sprite-image");
+var widthOfSpriteSheet = 832; //sbagliati
+var widthOfEachSprite = 832 / 13;
+var heightOfEachSprite = 64; //px
 
-    var animationInterval;
-    // var spriteSheet = document.getElementById("sprite-image");
-    var widthOfSpriteSheet = 832; //sbagliati
-    var widthOfEachSprite = 832 / 13;
-    var heightOfEachSprite = 64; //px
+function stopAnimation() {
+  clearInterval(animationInterval);
+}
 
-    function stopAnimation() {
-      clearInterval(animationInterval);
+//List of movements
+spriteMovements = {
+  walkDx: {
+    row: 10, //every movement has some properties
+    numOfImg: 9,
+  },
+  walkSx: {
+    row: 12,
+    numOfImg: 9,
+  },
+};
+
+// ANIMATIONS FUNCTION___________________________________________________________________
+
+function startAnimation(movement) {
+  var position = widthOfEachSprite; //start position for the image
+  const speed = 100; //in millisecond(ms)
+  const diff = widthOfEachSprite; //difference between two sprites
+
+  animationInterval = setInterval(() => {
+    spriteSheet.style.backgroundPosition = `-${position}px ${heightOfEachSprite * movement.row}px`;
+
+    if (position < widthOfSpriteSheet) {
+      position = position + diff;
+    } else {
+      //increment the position by the width of each sprite each time
+      position = widthOfEachSprite;
     }
-
-    //List of movements
-    export const spriteMovements = {
-      walkDx: {
-        row: 10, //every movement has some properties
-        numOfImg: 9,
-      },
-      walkSx: {
-        row: 12,
-        numOfImg: 9,
-      },
-    };
-
-    // ANIMATIONS FUNCTION___________________________________________________________________
-
-    export function startAnimation(movement,spriteSheet) {
-      var position = widthOfEachSprite; //start position for the image
-      const speed = 100; //in millisecond(ms)
-      const diff = widthOfEachSprite; //difference between two sprites
-
-      animationInterval = setInterval(() => {
-        spriteSheet.style.backgroundPosition = `-${position}px ${heightOfEachSprite * movement.row}px`;
-
-        if (position < widthOfSpriteSheet) {
-          position = position + diff;
-        } else {
-          //increment the position by the width of each sprite each time
-          position = widthOfEachSprite;
-        }
-        if (position == widthOfEachSprite * movement.numOfImg) {
-          //
-          position = widthOfEachSprite;
-        }
-        //reset the position to show first sprite after the last one
-      }, speed);
+    if (position == widthOfEachSprite * movement.numOfImg) {
+      //
+      position = widthOfEachSprite;
     }
+    //reset the position to show first sprite after the last one
+  }, speed);
+}
+
+//___________________________________________________________________
+//Start animation
+startAnimation(spriteMovements.walkSx);
